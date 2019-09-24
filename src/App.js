695bfa1330660c136d42ba6b2763655eb303987f
ArthurBArre <arthur_barre@outlook.fr>
2019-09-24 17:01:23 +0200

    commit of my first action with redux on react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div>Age: <span>{this.props.age}</span></div>
+        <button onClick={this.props.onAgeUp}>Age Up</button>
+        <button onClick={this.props.onAgeDown}>Age Down</button>
+      </div>
+    );
+  }
 }
-
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    age: state.age
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAgeUp: () => dispatch({ type: "AGE_UP" }),
+    onAgeDown: () => dispatch({ type: "AGE_DOWN" })
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
